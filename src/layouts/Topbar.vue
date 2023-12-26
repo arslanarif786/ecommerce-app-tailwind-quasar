e036ce5635fe98b68bb58489cc0d78553f006f35
Bojan Pavlovic <bojan@pitchstonetechnology.com>
2023-12-26 19:24:05 +0100

    finish login and register form

--- src/layouts/Topbar.vue
+++ src/layouts/Topbar.vue
@@ -1,3 +1,6 @@
+<script setup>
+const menu = ["WOMEN", "MEN", "BOYS", "GIRLS", "BRANDS", "HELP", "NEW IN", "CLOTHES", "SHOES", "BAGS", "ACCESSORIES", "BEAUTY", "OUTLET"]
+</script>
 <template>
     <q-bar :style="{ backgroundColor: '#2c2c33', color: '#fff' }">
         <RouterLink to="#" class="text-sm/[20px]">Sign Up for Newsletter</RouterLink>
@@ -10,36 +13,11 @@
         :style="{ backgroundColor: 'white', color: '#2a2b32', height: '71px', position: 'sticky', top: '0', 'background-color': 'white', 'z-index': 100 }"
         class="text-center">
         <q-list dense inline padding class="rounded-borders" style="display: flex; flex-direction: row">
-            <q-item clickable v-ripple>
-                <q-item-section class="hover:text-white hover:bg-[#2a2b32]">
-                    WOMEN
-                </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple>
-                <q-item-section class="hover:text-white hover:bg-[#2a2b32]">
-                    MEN
-                </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple>
-                <q-item-section class="hover:text-white hover:bg-[#2a2b32]">
-                    BOYS
-                </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple>
-                <q-item-section class="hover:text-white hover:bg-[#2a2b32]">
-                    GIRLS
-                </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple>
-                <q-item-section class="hover:text-white hover:bg-[#2a2b32]">
-                    BRANDS
-                </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple>
-                <q-item-section class="hover:text-white hover:bg-[#2a2b32]">
-                    HELP
-                </q-item-section>
-            </q-item>
+            <q-item  v-for="(item, index) in menu.slice(0, 5)" :key="index" v-ripple clickable>
+                    <q-item-section class="q-item-label hover:bg-[#2c2c33] hover:text-white">
+                        <RouterLink to="#">{{ item }}</RouterLink>
+                    </q-item-section>
+                </q-item>
         </q-list>
         <q-toolbar-title class="q-flex q-items-center">
             <RouterLink to="/">
@@ -62,43 +40,12 @@
     <q-toolbar class="bg-black text-gray-300 text-center" :style="{ position: 'sticky', top: '71px', backgroundColor: 'white', zIndex: 100 }">
         <q-toolbar-title>
             <q-list class="flex flex-row justify-center text-sm/[20px]">
-                <q-item v-ripple clickable>
+                <q-item v-for="(item, index) in menu.slice(6, 12)" :key="index" v-ripple clickable>
                     <q-item-section class="q-item-label hover:text-white">
-                        <RouterLink to="#">NEW IN</RouterLink>
-                    </q-item-section>
-                </q-item>
-                <q-item v-ripple clickable>
-                    <q-item-section class="q-item-label hover:text-white">
-                        <RouterLink to="#">CLOTHES</RouterLink>
-                    </q-item-section>
-                </q-item>
-                <q-item v-ripple clickable>
-                    <q-item-section class="q-item-label hover:text-white">
-                        <RouterLink to="#">SHOES</RouterLink>
-                    </q-item-section>
-                </q-item>
-                <q-item v-ripple clickable>
-                    <q-item-section class="q-item-label hover:text-white">
-                        <RouterLink to="#">BAGS</RouterLink>
-                    </q-item-section>
-                </q-item>
-                <q-item v-ripple clickable>
-                    <q-item-section class="q-item-label hover:text-white">
-                        <RouterLink to="#">ACCESSORIES</RouterLink>
-                    </q-item-section>
-                </q-item>
-                <q-item v-ripple clickable>
-                    <q-item-section class="q-item-label hover:text-white">
-                        <RouterLink to="#">BEAUTY</RouterLink>
-                    </q-item-section>
-                </q-item>
-                <q-item v-ripple clickable>
-                    <q-item-section class="q-item-label hover:text-white">
-                        <RouterLink to="#">OUTLET</RouterLink>
+                        <RouterLink to="#">{{ item }}</RouterLink>
                     </q-item-section>
                 </q-item>
             </q-list>
         </q-toolbar-title>
-    </q-toolbar></template>
-
-<script setup></script>
+    </q-toolbar>
+</template>
--- src/layouts/YellowSection.vue
+++ src/layouts/YellowSection.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="bg-[#ffff00] min-h-40 p-20">
         <div class="flex flex-row justify-center">
-            <span style="margin-right: 8px;" class="pt-5"><strong>SUBSCRIBE TO OUR NEWSLETTER</strong></span>
-            <q-input outlined v-model="ph" placeholder="Enter Your Email Address" style="background-color: white; height: 50px; width: 320px; margin-right: 8px;" />
+            <span class="pt-5 mr-8"><strong>SUBSCRIBE TO OUR NEWSLETTER</strong></span>
+            <q-input outlined v-model="ph" placeholder="Enter Your Email Address" class="bg-white h-13 w-80 mr-3" />
             <q-btn style="background-color: #4e4e5e; color: white; height: 50px; width: 320px;" label="Check-in" />
+            <!-- <q-btn class="bg-gray-700 text-white h-50 w-80" label="Check-in" /> -->
         </div>
         <div class="flex flex-row justify-center pt-3">
-            <q-checkbox size="xs" v-model="right" style="background-color: white; margin-right: 3px;"/>
-            <span style="margin-left: 8px;" class="pt-1">I agree with </span>
-            <span style="margin-left: 8px;" class="pt-1"><RouterLink to="#" class="text-sm/[20px]" style="color: #3683cd;">the Privacy Policy</RouterLink></span>
+            <q-checkbox size="xs" v-model="right" class="bg-white mr-3" />
+            <span class="pt-1">I agree with </span>
+            <span class="pt-1 ml-1"><RouterLink to="#" class="text-sm/[20px] text-[#3683cd]">the Privacy Policy</RouterLink></span>
         </div>
     </div>
   </template>
