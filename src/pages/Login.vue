a89c429b54ddc151ba7bda4f2a75888f76547089
Bojan Pavlovic <bojan@pitchstonetechnology.com>
2023-12-27 22:59:41 +0100

    add border to login, register and forgot password page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,24 +1,26 @@
 <template>
-      <q-form @submit.prevent="login" class="mt-5 flex flex-center flex-col">
-        <q-input
-          v-model="username"
-          label="Username"
-          dense
-          outlined
-          clearable
-          class="q-mb-md w-80 h-13"
-        />
-        <q-input
-          v-model="password"
-          label="Password"
-          type="password"
-          dense
-          outlined
-          clearable
-          class="q-mb-md w-80 h-13"
-        />
-        <q-btn type="submit" color="secondary" label="Login" class="q-mt-md w-80 h-13" />
-        <RouterLink to="/forgot-password" class="gt-xs text-sm/[20px] mt-5 mb-5"><strong>Forgot Password</strong></RouterLink>
+      <q-form @submit.prevent="login" class="m-5 flex flex-center flex-col">
+        <div class="px-[10px] py-[10px] rounded-md border border-gray-600">
+          <q-input
+            v-model="username"
+            label="Username"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-input
+            v-model="password"
+            label="Password"
+            type="password"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-btn type="submit" color="secondary" label="Login" class="q-mt-md w-80 h-13" />
+          <RouterLink to="/forgot-password" class="gt-xs text-sm/[20px] mx-5 mt-5 mb-5"><strong>Forgot Password</strong></RouterLink>
+        </div>
       </q-form>
   </template>
   <script setup>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,58 +1,60 @@
 <template>
-    <q-form @submit.prevent="register" class="mt-5 flex flex-center flex-col">
-      <q-input
-        v-model="username"
-        label="Username"
-        dense
-        outlined
-        clearable
-        class="q-mb-md w-80 h-13"
-      />
-      <q-input
-        v-model="email"
-        label="E-mail"
-        dense
-        outlined
-        clearable
-        class="q-mb-md w-80 h-13"
-      />
-      <q-input
-        v-model="phone"
-        label="Phone"
-        dense
-        outlined
-        clearable
-        class="q-mb-md w-80 h-13"
-      />
-      <q-input
-        v-model="address"
-        label="Address"
-        dense
-        outlined
-        clearable
-        class="q-mb-md w-80 h-13"
-      />
-      <q-input
-        v-model="password"
-        label="Password"
-        type="password"
-        dense
-        outlined
-        clearable
-        class="q-mb-md w-80 h-13"
-      />
-      <q-input
-        v-model="confirmPassword"
-        label="Confirm Password"
-        type="password"
-        dense
-        outlined
-        clearable
-        class="q-mb-md w-80 h-13"
-      />
-      <q-btn type="submit" color="secondary" label="Register" class="q-mt-md w-80 h-13" />
-      <span class="pt-2"><q-checkbox size="xs" v-model="right" /> I agree with <RouterLink to="#" class="text-sm/[20px] text-[#3683cd]">the Privacy Policy</RouterLink></span>
-      <RouterLink to="/login" class="gt-xs text-sm/[20px] mt-5 mb-5"><strong>Login</strong></RouterLink>
+    <q-form @submit.prevent="register" class="m-5 flex flex-center flex-col">
+      <div class="px-[10px] py-[10px] rounded-md border border-gray-600 flex-col">
+          <q-input
+            v-model="username"
+            label="Username"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-input
+            v-model="email"
+            label="E-mail"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-input
+            v-model="phone"
+            label="Phone"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-input
+            v-model="address"
+            label="Address"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-input
+            v-model="password"
+            label="Password"
+            type="password"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-input
+            v-model="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            dense
+            outlined
+            clearable
+            class="q-mb-md w-80 h-13"
+          />
+          <q-btn type="submit" color="secondary" label="Register" class="q-mt-md w-80 h-13 mb-5" />
+          <span class="pt-2"><q-checkbox size="xs" v-model="right" /> I agree with <RouterLink to="#" class="text-sm/[20px] text-[#3683cd]">the Privacy Policy</RouterLink></span>
+          <br><RouterLink to="/login" class="gt-xs text-sm/[20px]"><strong>If You Have An Account, Please Login</strong></RouterLink>
+    </div>
     </q-form>
 </template>
 <script setup>
@@ -68,6 +70,15 @@ const phone = ref('')
 const address = ref('')
 
 const register = () => {
-  console.log('Register with:', { username: username.value, password: password.value, confirmPassword: confirmPassword.value, email: email.value, phone: phone.value, address: address.value, right: right.value})
+  console.log('Register with:', { 
+    username: username.value, 
+    password: password.value, 
+    confirmPassword: 
+    confirmPassword.value, email: 
+    email.value, phone: 
+    phone.value, 
+    address: address.value, 
+    right: right.value
+  })
 }
 </script>
